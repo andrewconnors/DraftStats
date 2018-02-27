5db35a1075515c51add73ddca2e55fccc184f35d
Andrew Connors <connorsandrew1@gmail.com>
2018-02-26 22:04:40 -0400

    Fixes for names and years

--- hockeyreference/hockeyreference/spiders/draftposition.py
+++ hockeyreference/hockeyreference/spiders/draftposition.py
@@ -8,22 +8,23 @@ class DraftPositionSpider(scrapy.Spider):
     name = 'draftposition'
     allowed_domains = ['hockey-reference.com']
     years = [x for x in range(1979, 2017)]
-    start_urls = ['http://hockey-reference.com/draft/NHL_' + str(year) + '_entry.html'
-        for year in years
-    ]
+    def start_requests(self):
+        url = 'http://hockey-reference.com/draft/NHL_'
+        for year in self.years:
+            yield scrapy.Request(url + str(year) + '_entry.html', meta={'year': year})
 
 
     def parse(self, response):
-        player_names = response.css('td[data-stat="player"]::text').extract()
+        player_names = response.css('td[data-stat="player"]>a::text').extract()
         overall_ranking = filter(lambda x: x != 'Overall', response.css('th[data-stat="pick_overall"]::text').extract())
         games_played = response.css('td[data-stat="games_played"]::text').extract()
         amateur_team = response.css('td[data-stat="amateur_team"]::text').extract()
 
-        draftdata = {'Year': Series(self.years), 'Name': Series(player_names), 'Overall Pick': Series(overall_ranking),
+        draftdata = {'Year': response.meta['year'], 'Name': Series(player_names), 'Overall Pick': Series(overall_ranking),
         'GP': Series(games_played), 'Amateur Team': Series(amateur_team)}
         hockeydf = DataFrame(draftdata)
 
-        hockeydf.to_csv('draftdata.csv')
+        hockeydf.to_csv('./DraftData/draftdata-' + str(response.meta['year']) + '.csv')
         # with open('referenceurls.txt', 'a') as f:
         #     for name in playerNames:
         #         f.write(name + '\n')
